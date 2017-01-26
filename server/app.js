5558c0e2e5ed92b33b175a1aa2c13d795b602e8d
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-01-26 23:16:04 +1000

    feat(server): catch all server routes

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(allowCrossDomain);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '../index.html'));
 });
 
