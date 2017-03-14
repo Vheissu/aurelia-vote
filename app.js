ef457443de835c707b872e530cb1520313df210e
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-03-14 22:29:37 +1000

    feat(server): getting server setup

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get('/api/stories/:count/:offset', (req, res) => {
 });
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + 'public/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 // catch 404 and forward to error handler
